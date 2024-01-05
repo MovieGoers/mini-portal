4fb21857d3383885598e3bcb916a7923f4a92e0b
MovieGoers <alscjf4104@gmail.com>
2024-01-05 15:57:26 +0900

    feat : 위치 변환 기능 추가.

--- Assets/PortalScript.cs
+++ Assets/PortalScript.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject orangePortal;
+    public GameObject bluePortal;
+
+    Vector3 playerEnterVelocity;
+
+    bool m_EnteredPortal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_EnteredPortal = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerEnterVelocity = player.GetComponent<Rigidbody2D>().velocity;
+
+        bool isTouchingOrange = Physics2D.IsTouching(player.GetComponent<BoxCollider2D>(), orangePortal.GetComponent<BoxCollider2D>());
+        bool isTouchingBlue = Physics2D.IsTouching(player.GetComponent<BoxCollider2D>(), bluePortal.GetComponent<BoxCollider2D>());
+
+        if (isTouchingOrange && !m_EnteredPortal)
+        {
+            m_EnteredPortal = true;
+            player.transform.position = bluePortal.transform.position;
+        }
+
+        if (isTouchingBlue && !m_EnteredPortal)
+        {
+            m_EnteredPortal = true;
+            player.transform.position = orangePortal.transform.position;
+        }
+
+        if (!isTouchingBlue && !isTouchingOrange)
+        {
+            m_EnteredPortal = false;
+        }
     }
 }
