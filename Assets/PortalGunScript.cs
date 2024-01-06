e2266ca63b63a959c082bee470819ca207de8151
MovieGoers <alscjf4104@gmail.com>
2024-01-06 14:45:53 +0900

    feat : line 그리기

--- Assets/PortalGunScript.cs
+++ Assets/PortalGunScript.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PortalGunScript : MonoBehaviour
 {
+    public LineRenderer m_lineRenderer;
+    public GameObject m_player;
+
+    Vector3 m_startPos;
+    Vector3 m_endPos;
+    Vector3 m_mousePos;
+    Vector3 m_mouseDir;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,21 @@ public class PortalGunScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DrawAimLine();
+    }
+
+    void DrawAimLine()
+    {
+        m_mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        m_lineRenderer.enabled = true;
+
+        m_startPos = m_player.transform.position;
+        m_startPos.z = 0;
+        m_lineRenderer.SetPosition(0, m_startPos);
+
+        m_endPos = m_mousePos;
+        m_endPos.z = 0;
+        m_lineRenderer.SetPosition(1, m_endPos);
     }
 }
